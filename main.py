d18f105f3dae80dec749de2f7c199c8b94b90e6a
Jacklewis-123 <jack.lewis.2323th@gmail.com>
2024-09-07 15:07:10 +0100

    Adding pig

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify
+import random
+
+from animals.dog import Dog
+from animals.cat import Cat
+from animals.gerbil import Gerbil
+from animals.cow import Cow
+from animals.pig import Pig
+
+app = Flask(__name__)
+
+@app.route('/get_animal', methods=['POST'])
+def get_animal():
+    data = request.json
+    animal_type = data.get('type')
+    name = data.get('name')
+    
+    if animal_type.lower() == 'dog':
+        animal = Dog(name)
+    elif animal_type.lower() == 'cat':
+        animal = Cat(name)
+    elif animal_type.lower() == 'cow':
+        animal = Cow(name)
+    elif animal_type.lower() == 'gerbil':
+        animal = Gerbil(name)
+    elif animal_type.lower() == 'pig':
+        animal = Pig(name)    
+    else:
+        return jsonify({'error': 'Animal type not supported'}), 400
+    
+    return jsonify({
+        'name': animal.name,
+        'speak': animal.speak()
+    })
+
+# Existing imports and app setup...
+
+@app.route('/get_random_animal', methods=['GET'])
+def get_random_animal():
+    animals = [Dog('Rex'), Cat('Whiskers'), Gerbil('Gerry'), Cow('Cody'), Pig('Porky')]
+    animal = random.choice(animals)
+    
+    return jsonify({
+        'type': animal.__class__.__name__.lower(),
+        'name': animal.name,
+        'speak': animal.speak()
+    })
+
+# The rest of your Flask app...
+
+if __name__ == '__main__':
+    app.run(debug=True)

03f6c819a7f4ce21e184dd172cf0cbddaff4f3de
Adam Stevenson <adamjstevenson@hotmail.co.uk>
2024-09-08 10:06:05 +0100

    Adding docs / homepage

--- src/main.py
+++ src/main.py
@@ -1,72 +1,20 @@
 from flask import Flask, render_template, request, jsonify
 from flasgger import Swagger
 import random
+import os
 
 import animals
 
 app = Flask(__name__)
 
-swagger_config = {
-    "headers": [],
-    "specs": [
-        {
-            "endpoint": 'apispec_1',
-            "route": '/api-docs.json',
-            "rule_filter": lambda rule: True,  # all in
-            "model_filter": lambda tag: True,  # all in
-        }
-    ],
-    "static_url_path": "/flasgger_static",
-    "swagger_ui": True,
-    "specs_route": "/api-docs/"
-}
-
-
-swagger = Swagger(app, config=swagger_config)
-
 @app.route('/')
 def home():
-    routes = []
-    for rule in app.url_map.iter_rules():
-        routes.append({
-            'endpoint': rule.endpoint,
-            'methods': list(rule.methods),
-            'url': rule.rule
-        })
-    return render_template('index.html', routes=routes)
-
+    branch_name = '-' + os.getenv('BRANCH_NAME', '')
+    domain_name = os.getenv('DOMAIN_NAME', '')
+    return render_template('index.html')
 
 @app.route('/get_animal', methods=['POST'])
 def get_animal():
-    """
-    Get an animal by type and name
-    ---
-    parameters:
-      - name: type
-        in: body
-        type: string
-        required: true
-        description: The type of the animal
-      - name: name
-        in: body
-        type: string
-        required: true
-        description: The name of the animal
-    responses:
-      200:
-        description: A successful response
-        schema:
-          id: Animal
-          properties:
-            name:
-              type: string
-              description: The name of the animal
-            speak:
-              type: string
-              description: The sound the animal makes
-      400:
-        description: Invalid input
-    """
     data = request.json
     animal_type = data.get('type')
     name = data.get('name')
@@ -89,29 +37,8 @@ def get_animal():
         'speak': animal.speak()
     })
 
-# Existing imports and app setup...
-
 @app.route('/get_random_animal', methods=['GET'])
 def get_random_animal():
-    """
-    Get a random animal
-    ---
-    responses:
-      200:
-        description: A successful response
-        schema:
-          id: RandomAnimal
-          properties:
-            type:
-              type: string
-              description: The type of the animal
-            name:
-              type: string
-              description: The name of the animal
-            speak:
-              type: string
-              description: The sound the animal makes
-    """
     animal_list = [animals.Dog('Rex'), animals.Cat('Whiskers'), animals.Gerbil('Gerry'), animals.Cow('Cody'), animals.Pig('Porky')]
     animal = random.choice(animal_list)
     
@@ -123,19 +50,6 @@ def get_random_animal():
 
 @app.route('/health', methods=['GET'])
 def health():
-    """
-    Health check endpoint
-    ---
-    responses:
-      200:
-        description: A successful response
-        schema:
-          id: Health
-          properties:
-            status:
-              type: string
-              description: The health status
-    """
     return jsonify({'status': 'healthy'})
 
 if __name__ == '__main__':
